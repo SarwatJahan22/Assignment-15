fa397b834b64987aec8d147834b5ef7f09960e7b
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-07-06 17:16:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//More Guests: You just found a bigger dinner table, so now more space is available. 
+//Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15. 
+//Add a print statement to the end of your program informing people that you found a bigger dinner table.
+var guestArr = ["Zahra", "Zuhaib", "Zayan", "Ayesha"];
+var canNotAttend = "Zahra";
+var newGuest = "Ibrahim";
+guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+console.log(guestArr);
+guestArr.map((items));
+console.log("Dear ".concat(items, ", I found a bigger dinner table so I am invited more peoples."));
---
+++ main.ts
@@ -0,0 +1,18 @@
+//More Guests: You just found a bigger dinner table, so now more space is available. 
+//Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15. 
+//Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+let guestArr : string[] = ["Zahra", "Zuhaib", "Zayan", "Ayesha"];
+let canNotAttend : string = "Zahra"
+
+let newGuest : string = "Ibrahim"
+
+guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+
+console.log(guestArr)
+
+guestArr.map((guestArr));
+
+console.log(`Dear ${canNotAttend}, I found a bigger dinner table so I am invited more peoples.`);
+
